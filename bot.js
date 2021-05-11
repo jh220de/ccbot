12965fe4ab2e0dc0f3493a484a3d7e6f8c04960d
JH220 aka. Jan-Hendrik <50051569+JH220@users.noreply.github.com>
2021-05-11 21:34:47 +0200

    Update

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('message', message => {
     }
 
     try {
-		command.execute(message, args);
+		command.execute(message.toLowerCase, args);
 	} catch (error) {
         console.error(error);
 		message.reply("there was an error trying to execute that command!");
