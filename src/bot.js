b8a03da529ce7d342453b362459eda49cdfd52d2
JH220 <admin@jh220.de>
2021-06-26 17:32:46 +0200

    Update

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,12 @@ const start = new Date().getMilliseconds();
 
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json')
+const { prefix, token } = require('../config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
