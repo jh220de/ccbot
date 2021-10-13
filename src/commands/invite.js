7ed0ff503226c6b998d06ee629cfc9bc708227d4
JH220 aka. Jan-Hendrik <50051569+JH220@users.noreply.github.com>
2021-10-13 12:25:08 +0200

    Better formating

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -10,13 +10,8 @@ module.exports = {
             .setColor('00FFFF')
             .setTitle("ClearChat-Bot Invite")
             .setDescription(`
-Here's an invite link so you too can get this bot on your server!
+[Here's an invite link so you too can get this bot on your server!](https://discord.com/oauth2/authorize?client_id=787789079227006976&permissions=484416&redirect_uri=https%3A%2F%2Fwww.jh220.de&scope=bot%20applications.commands)
 We are glad about any support.
-
-https://www.jh220.de/ccbot
-
-Here's the direct link, it's just one click!
-https://discord.com/oauth2/authorize?client_id=787789079227006976&permissions=484416&redirect_uri=https%3A%2F%2Fwww.jh220.de&scope=bot%20applications.commands
             `);
         return interaction.reply({ embeds: [embed], ephemeral: true });
     },
