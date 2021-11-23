67496a9cbd55a0810021d7f817f9ea50d6f59f52
JH220 <50051569+JH220@users.noreply.github.com>
2021-11-23 17:07:26 +0100

    Fixed permission overwrite errors

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -7,13 +7,18 @@ module.exports = {
         .addIntegerOption(option => option.setName('amount').setDescription("Number of messages to clear"))
         .addUserOption(option => option.setName('target').setDescription("Clear messages only from a specific user")),
     async execute(interaction) {
-        if (interaction.guild == null)
-            return interaction.reply({ content: "You can use this command only on servers!", ephemeral: true });
+        if (interaction.channel.type != 'GUILD_TEXT')
+            return interaction.reply({ content: "You can use this command only in text channels on servers!", ephemeral: true });
         if (!interaction.channel.permissionsFor(interaction.member).has('MANAGE_MESSAGES'))
             return interaction.reply({ content: "You do not have enough permissions to do this.", ephemeral: true });
         
-        if (!interaction.channel.permissionsFor(interaction.guild.me).has('MANAGE_MESSAGES'))
-            return interaction.reply({ content: "The bot has insufficient permissions to delete messages.", ephemeral: true });
+        const permissions = interaction.channel.permissionsFor(interaction.guild.me);
+        if (!permissions.has('VIEW_CHANNEL'))
+            return interaction.reply({ content: "The bot has insufficient permissions to view this channel.", ephemeral: true });
+        if (!permissions.has('READ_MESSAGE_HISTORY'))
+            return interaction.reply({ content: "The bot has insufficient permissions to read channel history.", ephemeral: true });
+        if (!permissions.has('MANAGE_MESSAGES'))
+            return interaction.reply({ content: "The bot has insufficient permissions to manage messages.", ephemeral: true });
 
         var amount = interaction.options.getInteger('amount');
         if (!amount) amount = 100;
@@ -21,7 +26,7 @@ module.exports = {
 
         if (amount < 1 || amount > 100) return interaction.reply({ content: "You need to input a number between 1 and 100.", ephemeral: true });
 
-        await interaction.channel.messages.fetch({ limit: amount }).then(fetchMessages => {
+        interaction.channel.messages.fetch({ limit: amount }).then(fetchMessages => {
             var messages = fetchMessages.filter(message => !message.pinned);
             if (user) messages = messages.filter(message => message.author.id == user.id);
             interaction.channel.bulkDelete(messages, true).then(messages => {
--- src/commands/clearall.js
+++ src/commands/clearall.js
@@ -5,15 +5,25 @@ module.exports = {
         .setName('clearall')
         .setDescription("Clears ♻️ all messages in a channel"),
     async execute(interaction) {
-        if (interaction.guild == null)
-            return interaction.reply({ content: "You can use this command only on servers!", ephemeral: true });
+        if (interaction.channel.type != 'GUILD_TEXT')
+            return interaction.reply({ content: "You can use this command only in text channels on servers!", ephemeral: true });
         if (!interaction.channel.permissionsFor(interaction.member).has('MANAGE_CHANNELS'))
             return interaction.reply({ content: "You do not have enough permissions to do this.", ephemeral: true });
         
         if (!interaction.channel.permissionsFor(interaction.guild.me).has('MANAGE_CHANNELS'))
             return interaction.reply({ content: "The bot has insufficient permissions to manage channels.", ephemeral: true });
-
-            interaction.channel.clone();
-            interaction.channel.delete();
+        
+        if(interaction.channel.partial) {
+            if(!interaction.channel.parent.permissionsFor(interaction.guild.me).has('MANAGE_CHANNELS'))
+                return interaction.reply({ content: "The bot has insufficient permissions to manage channels.", ephemeral: true });
+        } else if(!interaction.guild.me.permissions.has('MANAGE_CHANNELS'))
+            return interaction.reply({ content: "The bot has insufficient permissions to manage channels.", ephemeral: true });
+        
+        await interaction.deferReply();
+        interaction.channel.delete();
+        interaction.channel.clone().then(channel => {
+            if(channel.permissionsFor(interaction.guild.me).has('SEND_MESSAGES'))
+                return channel.send(`Deleted all messages in this channel by ${interaction.user}.`);
+        });
     },
 };
