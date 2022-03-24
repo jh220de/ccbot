7ec59bb9d44d54836604ebddfd966f5a285e09c6
JH220 <admin@jh220.de>
2022-03-24 21:20:13 +0100

    Manually trigger ready after 30 seconds

--- bot.js
+++ bot.js
@@ -4,4 +4,5 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const event = require('./events/ready');
 client.once(event.name, (...args) => event.execute(...args));
 
+setTimeout(() => client.ws.connection.triggerReady(), 30000);
 client.login(require('./config.json').token);
