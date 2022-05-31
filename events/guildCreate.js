0920f65e968ccebd2bc1034c1975313b488f83dc
JH220 aka. Jan-Hendrik <50051569+JH220@users.noreply.github.com>
2022-05-31 16:51:55 +0000

    preventing some bugs

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,12 @@
 module.exports = {
 	name: 'guildCreate',
 	async execute(guild) {
-		if (!guild.id) return;
+		const serverId = guild.id;
+		if (!serverId) return;
 		const mysql = new (require ('../mysql'))();
-		const [rows] = mysql.getConnection().execute('SELECT * FROM `servers` WHERE serverId = ?', [guild.id]);
-		if (rows[0]) mysql.getConnection().execute('DELETE FROM `servers` WHERE serverId = ?', [guild.id]);
+		if (!mysql) return;
+		const [rows] = mysql.getConnection().execute('SELECT * FROM `servers` WHERE serverId = ?', [serverId]);
+		if (rows[0]) mysql.getConnection().execute('DELETE FROM `servers` WHERE serverId = ?', [serverId]);
 		mysql.updateGuild(guild);
 	},
 };
